674b2e208283c7eaf9b67fc56a819af8b37da357
Ruben Sierra <maguilag@gmail.com>
2014-08-18 15:16:07 +0200

    Install generator fixed .

--- lib/generators/lazy_data_api/install_generator.rb
+++ lib/generators/lazy_data_api/install_generator.rb
@@ -1,13 +1,13 @@
 require 'rails/generators'
 require 'rails/generators/active_record'
 
-module PaperTrail
+module LazyDataApi
   class InstallGenerator < ::Rails::Generators::Base
     include ::Rails::Generators::Migration
 
     source_root File.expand_path('../templates', __FILE__)
 
-    desc 'Generates (but does not run) a migration to add a api relations table.'
+    desc 'Generates (but does not run) a migration to add an api relations table.'
 
     def create_migration_file
       migration_template 'create_lazy_data_api_relations.rb', 'db/migrate/create_lazy_data_api_relations.rb'
