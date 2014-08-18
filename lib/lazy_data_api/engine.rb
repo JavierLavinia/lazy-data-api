0462f86e5ee2b3beea71dfbd0ffd13ead40e4849
Ruben Sierra <maguilag@gmail.com>
2014-08-18 15:18:20 +0200

    Rake problems fixed.

--- lib/lazy_data_api/engine.rb
+++ lib/lazy_data_api/engine.rb
@@ -2,17 +2,7 @@
 module LazyDataApi
 
   class Engine < Rails::Engine
-
-    initialize "lazy_data_api.load_app_instance_data" do |app|
-      LazyDataApi.setup do |config|
-        config.app_root = app.root
-      end
-    end
-
-    initialize "lazy_data_api.load_static_assets" do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/public"
-    end
-
+    isolate_namespace LazyDataApi
   end
 
 end
