86c6251018b7b3e669a8a03921670faf007c3898
Ruben Sierra <maguilag@gmail.com>
2014-08-18 17:43:20 +0200

    Initial tests.

--- test/lazy_data_api_test.rb
+++ test/lazy_data_api_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class LazyDataApiTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, LazyDataApi
-  end
+  test "should_be_apiable" do
+      Page.lazy_data
+
+      assert Page.apiable?
+    end
+
+    test "should_have_api_id" do
+      Page.lazy_data
+
+      @page = Page.new
+      assert_respond_to @page, :api_id
+    end
 end
