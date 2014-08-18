6af95456d797f853df158cc90fb38133624a7067
Ruben Sierra <maguilag@gmail.com>
2014-08-18 17:42:16 +0200

    Sintax and relation validations.

--- app/models/lazy_data_api/lazy_data_api_relation.rb
+++ app/models/lazy_data_api/lazy_data_api_relation.rb
@@ -1,6 +1,8 @@
 module LazyDataApi
   class Relation < ActiveRecord::Base
     self.table_name_prefix = 'lazy_data_api_'
-    belongs_to :apiable, :polymorphic => true
+    belongs_to :apiable, polymorphic: true
+
+    validate :api_id, uniqueness: true, present: true
   end
 end
