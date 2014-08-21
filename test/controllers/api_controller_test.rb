580f4b4ff0cfcac6150493853e57a0be7457b83d
Ruben Sierra <maguilag@gmail.com>
2014-08-21 16:41:43 +0200

    Check for wrong api requests.

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -9,4 +9,19 @@ class LazyDataApi::ApiControllerTest < ActionController::TestCase
     assert_not_nil assigns(:resource)
     assert_equal assigns(:resource), dummy
   end
+
+  test "should get error with worng api id lazy model" do
+    dummy = create :lazy_dummy
+    get :show, resource_name: :lazy_dummy, api_id: dummy.api_id.reverse
+
+    assert_response :not_found
+    assert !assigns(:resource)
+  end
+
+  test "should get error with no lazy model" do
+    get :show, resource_name: :no_lazy_dummy, api_id: ''
+
+    assert_response :not_found
+    assert !assigns(:resource)
+  end
 end
