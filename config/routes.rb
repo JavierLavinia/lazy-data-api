f22168e573f74df5b1b2d8e973747e00b0e47106
Ruben Sierra <maguilag@gmail.com>
2014-08-21 10:18:45 +0200

    Controller tests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :lazy_data_api, path: 'lazy-data-api', defaults: {format: :json} do
-    get ":resource/:api_id" => "lazy_data_api/api#show" , as: :lazy_data_api
+  namespace :lazy_data_api, defaults: {format: :json} do
+    get ":resource_name/:api_id" => "api#show"
   end
 end
