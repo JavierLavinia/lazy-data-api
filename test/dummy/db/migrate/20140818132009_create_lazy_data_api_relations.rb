12762c9cbd9b2cc6ee27ed958ed009e7d46f63d5
Ruben Sierra <maguilag@gmail.com>
2014-08-25 15:22:55 +0200

    Better relation table:
    * No id needed.
    * No timestamps needed.
    * DB Index on api id.

--- test/dummy/db/migrate/20140818132009_create_lazy_data_api_relations.rb
+++ test/dummy/db/migrate/20140818132009_create_lazy_data_api_relations.rb
@@ -1,11 +1,11 @@
 class CreateLazyDataApiRelations < ActiveRecord::Migration
   def self.up
-    create_table :lazy_data_api_relations do |t|
+    create_table :lazy_data_api_relations, id: false do |t|
       t.string :api_id
       t.integer :apiable_id
       t.string  :apiable_type
-      t.timestamps
     end
+    add_index :lazy_data_api_relations, :api_id
   end
 
   def self.down
