dc1b358858dbcad88a0a2b766e7a37b4cdf6e1bb
Ruben Sierra <maguilag@gmail.com>
2014-09-02 16:56:12 +0200

    Hash instead array for server options.

--- app/helpers/lazy_data_api/buttons_helper.rb
+++ app/helpers/lazy_data_api/buttons_helper.rb
@@ -2,11 +2,11 @@ module LazyDataApi
   module ButtonsHelper
     def send_lazy_data resource, options = {}
       content_tag :div, class: 'send-lazy-data' do
-        resource.api_servers.collect do |api_server|
+        resource.api_servers.each do |name, url_options|
           namespaces = resource.class.name.deconstantize.downcase.gsub('::','/')
           resource_name = resource.class.name.demodulize.downcase
-          url_options = api_server[:url_options].merge namespaces: namespaces, resource_name: resource_name, api_id: resource.api_id
-          concat button_to(t(".send_lazy_data.#{api_server[:name]}"), lazy_data_api.create_resource_url(url_options), options)
+          url_options = url_options.merge namespaces: namespaces, resource_name: resource_name, api_id: resource.api_id
+          concat button_to(t(".send_lazy_data.#{name}"), lazy_data_api.create_resource_url(url_options), options)
         end
       end
     end
--- lib/lazy_data_api/options.rb
+++ lib/lazy_data_api/options.rb
@@ -3,10 +3,10 @@ module LazyDataApi
 
     class_attribute :api_servers
 
-    self.api_servers = []
+    self.api_servers = {}
 
     def self.server name, url_options
-      api_servers << { name: name, url_options: url_options }
+      api_servers[name] = url_options
     end
   end
 end
