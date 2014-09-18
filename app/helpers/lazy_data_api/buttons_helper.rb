6b55344bb11617d2d40d8f668e1e7fb281a57b33
Ruben Sierra <maguilag@gmail.com>
2014-09-18 10:42:44 +0200

    Fixed classes name calculations.

--- app/helpers/lazy_data_api/buttons_helper.rb
+++ app/helpers/lazy_data_api/buttons_helper.rb
@@ -3,8 +3,8 @@ module LazyDataApi
     def send_lazy_data resource, options = {}
       content_tag :div, class: 'send-lazy-data' do
         resource.api_servers.each do |name, url_options|
-          namespaces = resource.class.name.deconstantize.downcase.gsub('::','/')
-          resource_name = resource.class.name.demodulize.downcase
+          namespaces = resource.class.name.deconstantize.underscore.gsub('::','/')
+          resource_name = resource.class.name.demodulize.underscore
           url_options = url_options.merge namespaces: namespaces, resource_name: resource_name, api_id: resource.api_id
           concat button_to(t(".send_lazy_data.#{name}", default: t("shared.send_lazy_data.#{name}")), lazy_data_api.create_resource_url(url_options), options)
         end
