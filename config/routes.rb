9324a5f5319553720777b459053678cac82c9fbf
Ruben Sierra <maguilag@gmail.com>
2014-10-01 19:42:23 +0200

    Fix: Routes reorder.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 LazyDataApi::Engine.routes.draw do
+  get "(/*namespaces)/:resource_name/:api_id/forward/:server_name" => "api#forward", as: 'forward_resource', defaults: {format: :json}
   get "(/*namespaces)/:resource_name/:api_id" => "api#show", as: 'show_resource', defaults: {format: :json}
-  get "(/*namespaces)/:resource_name/create/:api_id" => "api#create", as: 'create_resource', defaults: {format: :json}
+  post "(/*namespaces)/:resource_name/:api_id" => "api#create", as: 'create_resource', defaults: {format: :json}
 end
